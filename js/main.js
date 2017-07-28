8f8493e4e005d009332504b78f2c6e666fa16292
Anton Yessaulenko <avesaulenko.sbt@sberbank.ru>
2017-07-28 15:23:50 +0300

    Начал перерабатывать логику построения меню. Написал вспомогающие функции.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,58 @@
-(function(nav, plugins) {
+(function (nav) {
 
-    this.state = {
+    var props = {
         tree: nav.tree || null,
         productsList: nav.productsList || null
     }
+    this.state = {}
+
+    var noop = function () {
+    }
+
+    this.setState = function (newState, callback) {
+        newState = typeof newState === 'object' && newState !== null ? newState : {}
+        this.state = Object.assign(this.state, newState)
+
+        typeof callback === 'function' ? callback.apply(this) : void 0
+    }
+
+    this.createNode = function (tagName, attributes) {
+        attributes = attributes || {}
+        typeof tagName === 'string' ? void 0 : function () {
+            throw Error('Incorrect tag name in createNode method')
+        }
+        var node = document.createElement(tagName)
+
+        for (var property in attributes) {
+            if (attributes.hasOwnProperty(property)) {
+                node[property] = attributes[property]
+            }
+        }
+
+        return node
+    }
 
 
-    console.log(nav.tree === this.state.tree)
+    /* Рендер */
 
-    document.querySelector('#navigation').appendChild(menuBar)
+    function createMenuItem (title, url, hasPopup) {
 
-})(window.navigationData, { wade: Wade })
+    }
+
+
+    var menuBar = this.createNode('nav', {
+        'aria-label': 'Главное меню Сбербанк Онлайн',
+        'role': 'menubar'
+
+    })
+    var navigation = document.getElementById('navigation')
+    navigation.innerHTML = ''
+
+    this.setState({}, function (a, b) {
+        console.log(a + ' ' + b)
+    })
+
+
+    navigation.appendChild(menuBar)
+
+})(window.navigationData)
