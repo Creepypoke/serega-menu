a874e961ba63b33c99999c532a292de2b6b890bb
Anton <creepypoke@gmail.com>
2017-08-05 22:36:49 +0000

    CSS and design was improved. Contend was added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 (function (nav) {
-    var products = document.getElementById('products')
+
     var productsButton = document.getElementById('products-button')
-    var productsDashboardButtonCLose = document.getElementById('products-dashboard-close')
+    var productsDashboardButtonClose = document.getElementById('products-dashboard-close')
     var productsDashboard = document.getElementById('products-dashboard')
 
     var tabsList = document.getElementsByClassName('catalog-tab')
     var tabListWrapper = document.querySelector('[role="tablist"]')
-    
+
     var currentTab = 0
 
     var keys = {
@@ -16,7 +16,7 @@
         up: 38,
         right: 39,
         down: 40,
-        delete: 46
+        esc: 27
     }
 
     function forEach(collection, handler) {
@@ -42,51 +42,52 @@
         var isExpanded = productsDashboard.className.indexOf('hidden') !== -1
         productsDashboard.className = isExpanded ? 'dashboard' : 'dashboard hidden'
         productsDashboard.setAttribute('aria-hidden', !isExpanded)
-        productsDashboardButtonCLose.setAttribute('aria-expanded', !isExpanded)
+        productsDashboardButtonClose.setAttribute('aria-expanded', !isExpanded)
         productsButton.setAttribute('aria-expanded', isExpanded)
     }
-    
+
     function closeDashboard () {
         productsDashboard.className = 'dashboard hidden'
         productsDashboard.setAttribute('aria-hidden', true)
         productsButton.setAttribute('aria-expanded', false)
-        productsDashboardButtonCLose.setAttribute('aria-expanded', false)
-        
+        productsDashboardButtonClose.setAttribute('aria-expanded', false)
+
     }
 
-    function activeTab (tabElement) {
+    function activeTab (tabElement, withoutFocus) {
+        withoutFocus = withoutFocus || false
         var tabsList = document.getElementsByClassName('catalog-tab')
         var tabPanel = document.getElementById(tabElement.getAttribute('aria-controls'))
 
         forEach(tabsList, function(element) {
             if (element !== tabElement) {
                 element.setAttribute('aria-selected', 'false')
+                element.className = 'catalog-tab'
                 var tabPanel = document.getElementById(element.getAttribute('aria-controls'))
                 tabPanel.className = 'catalog-tab-panel hidden'
             }
         })
 
         tabElement.setAttribute('aria-selected', 'true')
+        if (!withoutFocus) {
+            tabElement.focus()
+        }
+        tabElement.className = 'catalog-tab catalog-tab_active'
         tabPanel.className = 'catalog-tab-panel'
 
     }
 
-    addClickListener(productsButton, toggleDashboard)
-    addClickListener(productsDashboardButtonCLose, closeDashboard)
-
-    forEach(tabsList, function(element) {
-        addClickListener(element, function() { activeTab(element) })
-    })
-
-    tabListWrapper.addEventListener('keyup', function(e) {
+    function changeTabFocus (e) {
         var key = e.keyCode
 
         switch(key) {
+            case keys.down:
             case keys.right:
                 if (currentTab < tabsList.length - 1) {
                     currentTab += 1
                 }
                 break
+            case keys.up:
             case keys.left:
                 if (currentTab > 0) {
                     currentTab -= 1
@@ -101,15 +102,31 @@
             default:
                 return e.preventDefault()
         }
-        
+
         var currentTabElement = tabsList[currentTab]
         if (currentTabElement !== void 0) {
             activeTab(tabsList[currentTab])
         }
+    }
+
+    addClickListener(productsButton, toggleDashboard)
+    addClickListener(productsDashboardButtonClose, closeDashboard)
+
+    window.addEventListener('keyup', function(e) {
+        if (productsDashboard.className !== 'dashboard hidden' && e.keyCode === keys.esc) {
+            console.log(e.keyCode)
+            closeDashboard()
+        }
     })
+
+    forEach(tabsList, function(element) {
+        addClickListener(element, function() { activeTab(element) })
+    })
+
+    tabListWrapper.addEventListener('keyup', changeTabFocus)
     
-    
-    
-    
+    if (tabsList[0] !== void 0) {
+        activeTab(tabsList[0], true)
+    }
 
 })(window.navigationData)
